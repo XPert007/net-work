1f8ecd508881e56c02cde8ffd6efb9f297460d0c
XPert007 <cookingcapybara@proton.me>
2025-12-03 10:48:40 +0530

    project complete, docs remaining

--- src/client.c
+++ src/client.c
@@ -1,13 +1,23 @@
 #include "../include/cns.h"
+
 int main(){
     int socketFD = createSocket();
     struct sockaddr_in *address = createAddress("127.0.0.1", 80);
-    int result = connect(socketFD,(struct sockaddr*)address, sizeof(*address)); 
+    int result = connect(socketFD, address, sizeof(*address));
     if(result == 0)
-        printf("connection was successful");
-    char* message;
-    message = "hello avi";
-    send(socketFD,message, strlen(message),0);
+        printf("connection was successful\n");
+
+    while(1){
+        char message[1024];
+        printf("enter the message you would like to send: ");
+        fgets(message, sizeof(message), stdin);
+
+        send(socketFD, message, strlen(message), 0);
+
+        char buffer[1024];
+        recv(socketFD, buffer, sizeof(buffer), 0);
+        printf("server replied: %s", buffer);
+    }
 
     return 0;
 }
